f2fd0cee5b616dd6b229ff791308ba2a7feca696
Kossin1337 <piotrswobodzinski@gmail.com>
2021-09-25 20:52:59 +0200

    Gif cleanup,  featured work implementation

--- src/components/FeaturedWork/FeaturedWork.js
+++ src/components/FeaturedWork/FeaturedWork.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./FeaturedWork.css";
 
 export const FeaturedWork = () => {
   return (
     <div className="fw-wrapper">
-      <h1>Featured work</h1>
+      <div className="fw">
+        <div className="fw-header">
+          <h2 className="fw-title">Featured work</h2>
+          <Link to="/projects">
+            <p className="fw-see-work">See all my work...</p>
+          </Link>
+        </div>
+        <div className="fw-content"></div>
+      </div>
     </div>
   );
 };
---
+++ src/components/FeaturedWork/featuredWorkData.js
@@ -0,0 +1,19 @@
+export const featuredWorkData = [
+  {
+    projectName: "CoinREX - Crypto Tracker",
+    description:
+      "Cryptocurrency price tracker with search functionality, routing using react-router, using various build-in and custom hooks, as well as Redux for state management. ",
+    technologies: ["React", "React-router", "SCSS", "Redux"],
+  },
+  {
+    projectName: "Color Picker",
+    description: "This app is an e",
+  },
+];
+
+export const technologiesColors = {
+  React: "",
+  "React-router": "",
+  Javascript: "",
+  SCSS: "",
+};
