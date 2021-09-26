dc013f9d8f3e462288d87806edac46d6db30da29
Kossin1337 <piotrswobodzinski@gmail.com>
2021-09-26 20:50:43 +0200

    Featured Projects implementation

--- src/components/FeaturedWork/FeaturedWork.js
+++ src/components/FeaturedWork/FeaturedWork.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { featuredWorkData } from "./featuredWorkData";
 
 import "./FeaturedWork.css";
 
@@ -9,11 +10,27 @@ export const FeaturedWork = () => {
       <div className="fw">
         <div className="fw-header">
           <h2 className="fw-title">Featured work</h2>
-          <Link to="/projects">
-            <p className="fw-see-work">See all my work...</p>
-          </Link>
         </div>
-        <div className="fw-content"></div>
+        <div className="fw-content">
+          {featuredWorkData.map((fw) => {
+            console.log(fw);
+            return (
+              <div className="project" key={fw.index}>
+                <div className="project-info">
+                  <h2 className="project-title">{fw.projectName}</h2>
+                  <p className="project-description">{fw.description}</p>
+                </div>
+                <div className="project-visualization">
+                  <img
+                    className="project-image"
+                    src={fw.image}
+                    alt={fw.projectName}
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/components/FeaturedWork/featuredWorkData.js
+++ src/components/FeaturedWork/featuredWorkData.js
@@ -1,19 +1,31 @@
+import CoinREX from "./images/CoinREX.jpg";
+import ColorPalette from "./images/ColorPalette.jpg";
+
 export const featuredWorkData = [
   {
+    index: 1,
     projectName: "CoinREX - Crypto Tracker",
     description:
       "Cryptocurrency price tracker with search functionality, routing using react-router, using various build-in and custom hooks, as well as Redux for state management. ",
-    technologies: ["React", "React-router", "SCSS", "Redux"],
+    link: "There goes a link to the project",
+    image: CoinREX,
   },
   {
+    index: 2,
     projectName: "Color Picker",
     description: "This app is an e",
+    technologies: ["React", "React-router", "SCSS", "Redux"],
+    link: "There goes a link to the project",
+    image: ColorPalette,
   },
 ];
 
+// Project utilizes CoinGecko API. It's Cryptocurrency price tracker with advanced search functionality.
+
 export const technologiesColors = {
-  React: "",
-  "React-router": "",
-  Javascript: "",
-  SCSS: "",
+  React: "#61DBFB",
+  "React router": "#72ECFC",
+  Javascript: "F0DB4F",
+  SCSS: "CC6699",
+  // Tailwind: "61DBFB",
 };
